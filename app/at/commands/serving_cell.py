1d9d201f72ffc46ce9ab1cbd5bd1f6c8cc37ef06
Nate Carlson <git@natecarlson.com>
2023-06-29 15:07:58 -0500

    Updating to match the current Quectel references.

--- app/at/commands/serving_cell.py
+++ app/at/commands/serving_cell.py
@@ -27,7 +27,7 @@ class ServingCellCommand(Command):
 
         # Clear the results
         self.results = []
-        
+
         if not cmd_result[0]:
             logger.warn("No response to ServingCell query")
             # No results to work with
@@ -59,8 +59,32 @@ class ServingCellCommand(Command):
             if tech_matches is None:
                 continue
 
+            if tech_matches.group(1) == 'NR5G-SA':
+                # NR5G Standalone
+                # "NR5G-SA",<duplex_mode>,<MCC>,<MNC>,<cellID>,<PCID>,<TAC>,<ARFCN>,<band>,<NR_DL_bandwidth>,<RSRP>,<RSRQ>,<SINR>,<scs>,<srxlev>
+                nr_params = tech_matches.group(2).split(",")
+                if len(nr_params) >= 14:
+                    self.results.append(ResultValue("nr_sa_duplex", "5GNR-SA Duplex", "Duplex mode - TDD or FDD", nr_params[0]))
+                    self.results.append(ResultValue("nr_sa_mcc", "5GNR-SA MCC", "5G Mobile Country Code", nr_params[1]))
+                    self.results.append(ResultValue("nr_sa_mnc", "5GNR-SA MNC", "5G Mobile Network Code", nr_params[2]))
+                    self.results.append(ResultValue("nr_sa_cellid", "5GNR-SA Cell ID", "5G Cell ID", nr_params[3]))
+                    self.results.append(ResultValue("nr_sa_pcid", "5GNR-SA PhyCell ID", "5G Physical Cell ID", nr_params[4]))
+                    self.results.append(ResultValue("nr_sa_tac", "5GNR-SA Tracking Area Code", "Tracking Area Code", nr_params[5]))
+                    self.results.append(ResultValue("nr_sa_arfcn", "5GNR-SA Cell ARFCN", "5G SA ARFCN", nr_params[6]))
+                    self.results.append(ResultValue("nr_sa_band", "5GNR-SA Band", "5G SA Band", nr_params[7]))
+                    # Add lookup table, per docs.
+                    self.results.append(ResultValue("nr_dl_bandwidth", "5GNR-SA Download Bandwidth", "5G SA Download Bandwidth", nr_params[8]))
+                    self.results.append(ResultValue("nr_sa_rsrp", "5GNR-SA RSRP", "5G Signal Power (dBmW)", nr_params[9]))
+                    self.results.append(ResultValue("nr_sa_rsrq", "5GNR-SA RSRQ", "5G Signal Quality (dBmW)", nr_params[10]))
+                    self.results.append(ResultValue("nr_sa_sinr", "5GNR-SA SINR", "5G Signal:Noise+Intrf. Ratio", nr_params[11]))
+                    # Add lookup table, per docs.
+                    self.results.append(ResultValue("nr_sa_scs", "5GNR-SA SCS", "5G NR Sub-Carrier Space", nr_params[12]))
+                    self.results.append(ResultValue("nr_sa_srxlev", "5GNR-SA SRXLev", "5G Signal:Suitable Reception Level", nr_params[13]))
+
+
             if tech_matches.group(1) == 'NR5G-NSA':
                 # Process NR5G group
+                # "NR5G-NSA",<MCC>,<MNC>,<PCID>,<RSRP>,<SINR>,<RSRQ>,<ARFCN>,<band>,<NR_DL_bandwidth>,<scs>
                 nr_params = tech_matches.group(2).split(",")
                 if len(nr_params) >= 6:
                     self.results.append(ResultValue("nr_nsa_mcc", "5GNR-NSA MCC", "5G Mobile Country Code", nr_params[0]))
@@ -69,12 +93,18 @@ class ServingCellCommand(Command):
                     self.results.append(ResultValue("nr_nsa_rsrp", "5GNR-NSA RSRP", "5G Signal Power (dBmW)", nr_params[3]))
                     self.results.append(ResultValue("nr_nsa_sinr", "5GNR-NSA SINR", "5G Signal:Noise+Intrf. Ratio", nr_params[4]))
                     self.results.append(ResultValue("nr_nsa_rsrq", "5GNR-NSA RSRQ", "5G Signal Quality (dBmW)", nr_params[5]))
+                    self.results.append(ResultValue("nr_sa_arfcn", "5GNR-SA Cell ARFCN", "5G SA ARFCN", nr_params[6]))
+                    self.results.append(ResultValue("nr_sa_band", "5GNR-SA Band", "5G SA Band", nr_params[7]))
+                    # Add lookup table, per docs.
+                    self.results.append(ResultValue("nr_dl_bandwidth", "5GNR-SA Download Bandwidth", "5G SA Download Bandwidth", nr_params[8]))
+                    self.results.append(ResultValue("nr_sa_scs", "5GNR-SA SCS", "5G NR Sub-Carrier Space", nr_params[9]))
 
             elif tech_matches.group(1) == 'LTE':
                 # Process LTE group
+                # "LTE",<is_tdd>,<MCC>,<MNC>,<cellID>,<PCID>,<earfcn>,<freq_band_ind>,<UL_bandwidth>,<DL_bandwidth>,<TAC>,<RSRP>,<RSRQ>,<RSSI>,<SINR>,<CQI>,<tx_power>,<srxlev>
                 lte_params = tech_matches.group(2).split(",")
                 if len(lte_params) >= 14:
-                    self.results.append(ResultValue("lte_dup_type", "LTE Duplex", "LTE Duplex Mode", lte_params[0].replace("\"", "")))
+                    self.results.append(ResultValue("lte_duplex", "LTE Duplex", "LTE Duplex mode - TDD or FDD", lte_params[0].replace("\"", "")))
                     self.results.append(ResultValue("lte_mcc", "LTE MCC", "LTE Mobile Country Code", lte_params[1]))
                     self.results.append(ResultValue("lte_mnc", "LTE MNC", "LTE Mobile Network Code", lte_params[2]))
                     self.results.append(ResultValue("lte_cid", "LTE Cell ID", "LTE Cell ID", lte_params[3]))
@@ -88,9 +118,9 @@ class ServingCellCommand(Command):
                     self.results.append(ResultValue("lte_rsrq", "LTE RSRQ", "LTE Signal Quality (dB)", lte_params[11]))
                     self.results.append(ResultValue("lte_rssi", "LTE RSSI", "LTE Signal Strength (dBm)", lte_params[12]))
                     self.results.append(ResultValue("lte_sinr", "LTE SINR", "LTE Signal:Noise+Intrf. Ratio", lte_params[13]))
-                    # self.results.append(ResultValue("lte_cqi", "LTE CQI", "LTE Channel Quality Indicator", lte_params[14]))
-                    # self.results.append(ResultValue("lte_tx_power", "LTE TX Power", "LTE Transmit Power (dBmW)", lte_params[15]))
-
+                    self.results.append(ResultValue("lte_cqi", "LTE CQI", "LTE Channel Quality Indicator", lte_params[14]))
+                    self.results.append(ResultValue("lte_tx_power", "LTE TX Power", "LTE Transmit Power (dBmW)", lte_params[15]))
+                    self.results.append(ResultValue("nr_sa_srxlev", "LTE SRXLev", "LTE Signal:Suitable Reception Level", nr_params[13]))
             elif tech_matches.group(1) == 'WCDMA':
                 # Process UMTS group
                 wcdma_params = tech_matches.group(2).split(",")
@@ -109,5 +139,5 @@ class ServingCellCommand(Command):
                     self.results.append(ResultValue("wcdma_slot", "WCDMA Slot", "WCDMA Slot ID", wcdma_params[11]))
                     # self.results.append(ResultValue("wcdma_speech_code", "WCDMA Speech Code", "WCDMA Speech Code", wcdma_params[12]))
                     # self.results.append(ResultValue("wcdma_com_mode", "WCDMA Compression", "WCDMA Compression On/Off", wcdma_params[13]))
-                    
+
         self.last_update = time.time()
